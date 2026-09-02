13a52c65a8c78f9bfc558e1fceed77e00bd50db5
passant ahmed <passantahmed992006@outlook.com>
2026-09-03 02:42:34 +0300

    Q1

--- CS Basics04G03/Program.cs
+++ CS Basics04G03/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+#region 01
+double[] price = new double[3];
+
+price[0] = 25.5;
+price[1] = 40.0;
+price[2] = 33.75;
+{
+    Console.WriteLine(price[1]);
+}
+#endregion
